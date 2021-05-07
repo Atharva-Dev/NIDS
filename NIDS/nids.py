ff9fbe31a1971c72657fb6f81aa5ec79417f72b9
Atharva-Dev <atharvadev8@gmail.com>
2021-05-07 23:04:26 +0530

    packeg created

--- NIDS/nids.py
+++ NIDS/nids.py
@@ -7,35 +7,39 @@ apr_responces = defaultdict(lambda: 0)
 tcp_open_ports = 0
 tcp_open_port_threshold = 100
 my_mac = Ether().src
+arp_spoofed = False
+syn_flooded = False
 
 def set_tcp_open_port_threshold(threshold):
     global tcp_open_port_threshold
     tcp_open_port_threshold = threshold
 
 def check_syn_flood(packet) :
+    global syn_flooded
     global tcp_open_ports
     if packet['TCP'].flags.S:
         tcp_open_ports += 1
     if packet['TCP'].flags.A:
         tcp_open_ports -= 1
     if tcp_open_ports > tcp_open_port_threshold :
-        #todo alert !!
+        syn_flooded = True
     
 def varify_mac(target_ip):
     request_arp = scapy.ARP(pdst = target_ip)
     br = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
-    arp_req_br = br / arp_request
+    arp_req_br = br / request_arp
     list_1 = scapy.srp(arp_req_br, timeout=5,verbose=False)[0]
     return list_1[0][1].hwsrc
 
 def check_arp_spoofing(packet):
+    global arp_spoofed
     who_has = 1;
     is_at = 2;
     if packet['ARP'].op == is_at :
         received_mac = packet['ARP'].hwsrc
         checked_mac = varify_mac(packet['ARP'].psrc)
         if received_mac != checked_mac :
-            #todo alert !!
+            arp_spoofed = True
 
     
 def examine(packet):
