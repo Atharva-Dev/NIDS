aa923606382953dd456d9a90ae26a46444a278ec
Atharva-Dev <atharvadev8@gmail.com>
2021-05-07 20:33:37 +0530

    added syn flood attack detection

--- nids.py
+++ nids.py
@@ -1,10 +1,34 @@
 from __future__ import print_function
-from scapy.all import sniff
+from collections import defaultdict
+from scapy.all import sniff, Ether
 
-def examine_arp(packet):
-    print(packet.summary())
-    return
+
+apr_responces = defaultdict(lambda: 0)
+tcp_open_ports = 0
+tcp_open_port_threshold = 100
+my_mac = Ether().src
+
+def set_tcp_open_port_threshold(threshold):
+    global tcp_open_port_threshold
+    tcp_open_port_threshold = threshold
+
+def check_syn_flood(packet) :
+    global tcp_open_ports
+    if packet['TCP'].flags.S:
+        tcp_open_ports += 1
+    if packet['TCP'].flags.A:
+        tcp_open_ports -= 1
+    if tcp_open_ports > tcp_open_port_threshold :
+        #todo alert !!
+    
+
+    
+def examine(packet):
+    if my_mac != packet['Ether'].src :
+        if 'TCP' in packet : check_syn_flood(packet)
+        if 'ARP' in packet : check_arp_spoofing(packet)
 
 if __name__ == "__main__" :
-    sniff(filter='arp', prn=examine_arp)
+    sniff(filter='tcp or arp', prn=examine, store = False)
+    print(apr_responces)
 
