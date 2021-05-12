7b52af1e0ab1fb309bfddb5ca1624a2e5dc986d2
Atharva-Dev <atharvadev8@gmail.com>
2021-05-12 11:33:35 +0530

    app ui file deleted

--- NIDS/conn.py
+++ NIDS/conn.py
@@ -19,7 +19,7 @@ def get_vals(arp_spoof_checked, syn_flood_checked, max_open_conn):
     sniffer.detect_arp_spoofing = arp_spoof_checked
     sniffer.detect_syn_flood = syn_flood_checked
     print(sniffer.detect_arp_spoofing, sniffer.detect_syn_flood, max_open_conn)
-    if(sniffer.detect_syn_flood) : sniffer.set_tcp_open_port_threshold(max_open_conn)
+    if(sniffer.detect_syn_flood) : sniffer.set_tcp_open_port_threshold(int(max_open_conn))
     start_in_background()
 
 @eel.expose    
